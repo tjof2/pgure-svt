f9c414799922cae34b13193af79bcc04c85b1899
bm424 <bm424@cam.ac.uk>
2016-04-11 17:03:43 +0100

    Re-represent helper functions as a subclass

--- hyperspy_demo.py
+++ hyperspy_demo.py
@@ -1,27 +1,59 @@
+from pguresvt import SVT
+from hyperspy import signals
 
-def prepare_to_denoise(signal, spectral_axis=0):
-    """
-    Represents `signal` as a spectrum (three dimensions; relevant axis last)
-    then returns the data, properly aligned.
 
-    Parameters
-    ----------
-    signal : hyperspy.signals.Signal
-        The HyperSpy signal to prepare_to_denoise; can be of arbitrary
-        type/shape.
-    spectral_axis : Optional [int]
-        The axis along which denoising will occur; for example in a time
-        series of images with shape (285|256, 256) the spectral axis is 0.
+class HSPYSVT(SVT):
 
-    Returns
-    -------
-    signal_data : numpy.ndarray
+    """HyperSpy implementation of SVT.
 
+    Allows arbitrary HyperSpy signals to be passed for SVT denoising.
 
     """
-    signal_3d = signal.as_spectrum(spectral_axis=spectral_axis)
-    signal_data = signal_3d._data_aligned_with_axes
-    return signal_data
+
+    @staticmethod
+    def prepare_to_denoise(signal, spectral_axis=0):
+        """
+        Represents `signal` as a spectrum (three dimensions; relevant axis last)
+        then returns the data, properly aligned.
+
+        Parameters
+        ----------
+        signal : hyperspy.signals.Signal
+            The HyperSpy signal to denoise; can be of arbitrary type/shape.
+        spectral_axis : Optional [int]
+            The axis along which denoising will occur; for example in a time
+            series of images with shape (285|256, 256) the spectral axis is 0.
+
+        Returns
+        -------
+        signal_data : numpy.ndarray
+
+        """
+        signal_3d = signal.as_spectrum(spectral_axis=spectral_axis)
+        signal_data = signal_3d._data_aligned_with_axes
+        return signal_data
+
+    def denoise(self, signal, spectral_axis=0):
+        """
+        Denoises an arbitrary HyperSpy signal.
+
+        Parameters
+        ----------
+        signal : hyperspy.signals.Signal
+            The HyperSpy signal to denoise; can be of arbitrary type/shape.
+        spectral_axis : Optional [int]
+            The axis along which denoising will occur; for example in a time
+            series of images with shape (285|256, 256) the spectral axis is 0.
+
+        Returns
+        -------
+        hyperspy.signals.Spectrum
+            Denoised data as a spectrum.
+
+        """
+        x = self.prepare_to_denoise(signal, spectral_axis)
+        super(HSPYSVT, self).denoise(x)
+        return signals.Spectrum(self.Y)
 
 
 if __name__ == '__main__':
@@ -32,7 +64,7 @@ if __name__ == '__main__':
     movie = load("FEI_HAADF_Image_movie_282.dm4")
 
     # Rearrange into appropriate dimensions
-    data = prepare_to_denoise(movie, spectral_axis=0)
+    data = HSPYSVT.prepare_to_denoise(movie, spectral_axis=0)
 
     # Check new dimensions
     print(data.shape)
