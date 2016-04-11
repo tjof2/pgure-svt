a2090641bc8ab9009ac096731991f68126e5b4a6
tjof2 <tjof2@cam.ac.uk>
2016-04-11 08:34:55 +0100

    Added GNU GPL3 header to all code files

--- src/svt.hpp
+++ src/svt.hpp
@@ -9,6 +9,21 @@
             Spectral Estimators", (2013), Candes, EJ et al.
             http://dx.doi.org/10.1109/TSP.2013.2270464
 
+    This file is part of  PGURE-SVT.
+
+    PGURE-SVT is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PGURE-SVT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PGURE-SVT. If not, see <http://www.gnu.org/licenses/>.
+
 ***************************************************************************/
 
 #ifndef SVT_H
